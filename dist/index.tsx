68a45117023a18a120a29fa753cf6b2574908a6d
Hitenkumar Dineshkumar Patel <hpatel@primesoft.local>
2022-01-09 23:29:21 +0530

    initial commit

--- dist/index.tsx
+++ dist/index.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import { App } from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
+// import "./index.css";
+// import { App } from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import { Provider } from "react-redux";
+// import { store } from "./store/store";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+
+export { App } from "./App";
+export { store } from "./store/store";
+// export { TStore } from "./store/store";
+export { changeObj } from "./slices/example";
--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { App } from "./App";
-export { store } from "./store/store";
-export { TStore } from "./store/store";
-export { changeObj } from "./slices/example";
